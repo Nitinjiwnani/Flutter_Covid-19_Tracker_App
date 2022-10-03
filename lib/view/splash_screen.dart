255ad0a1b5130b265cba4e2f3dc5b1d3358879a9
Nitinjiwnani <jiwnani01@gmail.com>
2022-10-03 18:10:20 +0530

    world states screen with pie chart

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -61,7 +61,7 @@ class _SplashScreenState extends State<SplashScreen>
               ),
               builder: (BuildContext context, Widget? child) {
                 return Transform.rotate(
-                  angle: _controller.value * 2.0 * 3.14,
+                  angle: _controller.value * 2.0 * pi,
                   child: child,
                 );
               },
--- lib/view/world_states.dart
+++ lib/view/world_states.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStatesScreen extends StatefulWidget {
   const WorldStatesScreen({super.key});
@@ -8,9 +10,98 @@ class WorldStatesScreen extends StatefulWidget {
   State<WorldStatesScreen> createState() => _WorldStatesScreenState();
 }
 
-class _WorldStatesScreenState extends State<WorldStatesScreen> {
+class _WorldStatesScreenState extends State<WorldStatesScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  final colorList = <Color>[
+    const Color(0xff4285F4),
+    const Color(0xff1aa260),
+    const Color(0xffde5246),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .01,
+            ),
+            PieChart(
+              dataMap: {
+                "Total": 20,
+                "Recovered": 15,
+                "Deaths": 5,
+              },
+              chartRadius: MediaQuery.of(context).size.width / 3.2,
+              legendOptions:
+                  const LegendOptions(legendPosition: LegendPosition.left),
+              animationDuration: const Duration(milliseconds: 1200),
+              chartType: ChartType.ring,
+              colorList: colorList,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height * .06),
+              child: Card(
+                child: Column(
+                  children: [
+                    ReusableRow(title: 'Total', value: '200'),
+                    ReusableRow(title: 'Total', value: '200'),
+                    ReusableRow(title: 'Total', value: '200'),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xff1aa260),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Text('Track Countries'),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  String title, value;
+  ReusableRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 5),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title),
+              Text(value),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
